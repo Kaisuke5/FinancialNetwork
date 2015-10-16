384add594c79dd785c54d4045f9e502d9aba3f73
Shintaro Shiba <shintaro@Shintaro-no-MacBook-Air.local>
2015-10-16 18:25:27 +0900

    minor change

--- src/Training.py
+++ src/Training.py
@@ -27,6 +27,7 @@ from chainer import optimizers
 class Training:
     """ Training """
 
+    ## Data
     ## Set hyperparameters
     def __init__(self,dataname):
         self.dataname = dataname
@@ -39,7 +40,19 @@ class Training:
         self.train_ac,self.test_ac,self.train_mean_loss,self.test_mean_loss = [],[],[],[]
 
 
-    ## Standardization function
+    ## Data
+    ## Prepare multi-layer perceptron model: l1=input nodes, l3=output nodes
+    def setmodel(self):
+        self.model = chainer.FunctionSet(l1=F.Linear(self.n_input, self.n_units),
+                                    l2=F.Linear(self.n_units, self.n_units),
+                                    l3=F.Linear(self.n_units, self.n_output))
+        ## Setup optimizer
+        self.optimizer = optimizers.Adam()
+        self.optimizer.setup(self.model)
+
+
+    ## Method
+    ## Data standardization (only input side)
     def stdinp(self):
         self.M = np.mean(self.D['data'],axis=0)
         self.Sd = np.std(self.D['data'],axis=0)
@@ -51,7 +64,7 @@ class Training:
         self.D['data'] = np.array(input_s.T)
 
 
-    ## Prepare dataset
+    ## Method / Prepare dataset
     def load(self):
         print('load dataset pkl file')
         with open(self.dataname, 'rb') as D_pickle:
@@ -69,16 +82,7 @@ class Training:
         self.N_test = self.y_test.size
 
 
-    ## Prepare multi-layer perceptron model: l1=input nodes, l3=output nodes
-    def setmodel(self):
-        self.model = chainer.FunctionSet(l1=F.Linear(self.n_input, self.n_units),
-                                    l2=F.Linear(self.n_units, self.n_units),
-                                    l3=F.Linear(self.n_units, self.n_output))
-        ## Setup optimizer
-        self.optimizer = optimizers.Adam()
-        self.optimizer.setup(self.model)
-
-
+    ## Method
     ## Neural net architecture
     ## softmax and accuracy for discrimination task
     def forward(self, x_data, y_data, dropout, train=True):
@@ -89,12 +93,12 @@ class Training:
         return F.softmax_cross_entropy(y, t), F.accuracy(y, t)
 
 
+    ## Method / train
     def train(self):
         perm = np.random.permutation(self.N)
         sum_accuracy = 0
         sum_loss = 0
-        # batch loop
-        for i in six.moves.range(0, self.N, self.batchsize):
+        for i in six.moves.range(0, self.N, self.batchsize):                        # batch loop
             x_batch = np.asarray(self.x_train[perm[i:i + self.batchsize]])
             y_batch = np.asarray(self.y_train[perm[i:i + self.batchsize]])
             self.optimizer.zero_grads()
@@ -107,7 +111,7 @@ class Training:
         self.train_ac.append(sum_accuracy / self.N)
 
 
-    ## evaluation
+    ## Method / evaluation
     def evaluate(self):
         sum_accuracy = 0
         sum_loss = 0
@@ -120,8 +124,23 @@ class Training:
         self.test_mean_loss.append(sum_loss / self.N_test)
         self.test_ac.append(sum_accuracy / self.N_test)
 
-    ## LOGGING
-    def writelog(self,stime,etime):
+
+    ## Method / learning loop
+    def learnngloop(self):
+        for epoch in six.moves.range(0, self.n_epoch + 1):
+            print('epoch', epoch)
+            start_time = time.clock()
+            if epoch > 0:
+                self.train()
+                print('train mean loss={}, accuracy={}'.format(self.train_mean_loss[-1], self.train_ac[-1]))
+            self.evaluate()
+            print('test  mean loss={}, accuracy={}'.format(self.test_mean_loss[-1], self.test_ac[-1]))
+            end_time = time.clock()
+            print "\ttime = %.3f" %(end_time-start_time)
+
+
+    ## Method / LOGGING
+    def writelog(self,stime,etime,LOG_FILENAME):
         logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG, format='%(asctime)s %(message)s')
         logging.info('New trial.\nData: %s\nAll data: %d frames, train: %d frames / test: %d frames.\n   Layers = %d, Units= %d, Batchsize = %d,  Time = %.3f,  Dropout = %.3f\n   Epoch: 0,  test mean loss=  %.5f, accuracy=  %.5f\n   Epoch: %d, train mean loss=  %.5f, accuracy=  %.5f\n              test mean loss=  %.3f, accuracy=  %.3f\n',
                      self.dataname,self.N+self.N_test,self.N,self.N_test,self.Lay,self.n_units,self.batchsize,etime-stime,self.dropout,self.test_mean_loss[0], self.test_ac[0],self.n_epoch, self.train_mean_loss[-1], self.train_ac[-1],self.test_mean_loss[-1], self.test_ac[-1])
@@ -135,25 +154,16 @@ class Training:
 
 if __name__=="__main__":
     dataname = "AMZN5.pkl"
-    LOG_FILENAME = 'log_train.txt'
-    Data = Training(dataname)
+    LOG_FILENAME = 'log.txt'
 
+    Data = Training(dataname)
     Data.load()
     Data.setmodel()
 
     stime = time.clock()
-    for epoch in six.moves.range(0, Data.n_epoch + 1):
-        print('epoch', epoch)
-        start_time = time.clock()
-        if epoch > 0:
-            Data.train()
-            print('train mean loss={}, accuracy={}'.format(Data.train_mean_loss[-1], Data.train_ac[-1]))
-        Data.evaluate()
-        print('test  mean loss={}, accuracy={}'.format(Data.test_mean_loss[-1], Data.test_ac[-1]))
-        end_time = time.clock()
-        print "\ttime = %.3f" %(end_time-start_time)
-
+    Data.learnngloop()
     etime = time.clock()
-    Data.writelog(stime,etime)
+
+    Data.writelog(stime,etime,LOG_FILENAME)
 
 
